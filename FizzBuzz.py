f4337c81a8fd3246d33a54b57fed9cc12d090eee
Caden Verzino <verzinoc@oregonstate.edu>
2021-03-06 17:56:48 -0800

    Implemented x method: wrote code to increment 100 times from zero and concate the number or Fizz/Buzz/FizzBuzz to the counter string, depending if the number was multiple of 3 or 5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,16 @@
 def FizzBuzz():
-   return 0;
+
+   counter = ""
+   for x in range(100):
+      temp = x + 1
+      if ((temp) % 3) == 0 and ((temp) % 5) == 0:
+	 counter = counter + "FizzBuzz "
+      elif ((x+1)%3) == 0:
+	 counter = counter + "Fizz "
+      elif ((x+1)%5) == 0:
+	 counter = counter + "Buzz "
+      else:
+	 counter = counter + str(temp) + " "
+
+   return counter;
+
