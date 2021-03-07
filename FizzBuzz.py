21adb77d8221e7fc60066196095a2da6c7a5537f
Caden Verzino <verzinoc@oregonstate.edu>
2021-03-06 18:14:43 -0800

    Implemented x method: added code to pass test2 that verifies that 100 numbers/words are printed

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,10 @@
-def FizzBuzz():
+def FizzBuzz(option):
 
    counter = ""
+   count = 0
    for x in range(100):
       temp = x + 1
+      count = count + 1
       if ((temp) % 3) == 0 and ((temp) % 5) == 0:
 	 counter = counter + "FizzBuzz "
       elif ((x+1)%3) == 0:
@@ -12,5 +14,8 @@ def FizzBuzz():
       else:
 	 counter = counter + str(temp) + " "
 
-   return counter;
+   if option == 2:
+      return count
+   else:
+      return counter
 
