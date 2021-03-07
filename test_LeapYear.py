6a06b68d2df4e1b510661b5631e15c0a7dd45e05
Caden Verzino <verzinoc@oregonstate.edu>
2021-03-06 18:38:46 -0800

    Wrote test1 to calculate if 2004 is a leap year, anticipating return value 1 for TRUE

--- test_LeapYear.py
+++ test_LeapYear.py
@@ -3,6 +3,8 @@ import LeapYear
 
 class TestLeapYear(unittest.TestCase):
    def test_1(self):
+      result = LeapYear.LeapYear(2004)
+      self.assertEqual(result, 1)
 
 if __name__ == "__main__":
    unittest.main(verbosity=2)
